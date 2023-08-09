a8f7fc0642f1d28e54b51998ad29ca80e72735df
Ardeshir21 <examples21@gmail.com>
2023-08-09 20:59:03 +1000

    Initial commit

--- GiftQuests/settings.py
+++ GiftQuests/settings.py
@@ -39,7 +39,7 @@ ALLOWED_HOSTS = ['localhost',
                  '127.0.0.1',
                  # '161.35.103.31',
                  # 'www.turkeymode.com',
-                 #  'turkeymode.com',
+                 # 'turkeymode.com',
 ]
 
 # Application definition
